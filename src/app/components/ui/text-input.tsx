63edff788876fac26a79c84a94ba254838f8365e
Caio Vellani <caiovellani@outlook.com>
2025-01-11 13:33:26 -0300

    feat: Enhance Hero component and add UI elements
    - Add lucide-react dependency
    - Import and use UserCard component in Hero
    - Add TextInput and Button components to Hero
    - Adjust Hero layout and styling
    - Update TextInput component styles

--- src/app/components/ui/text-input.tsx
+++ src/app/components/ui/text-input.tsx
@@ -9,7 +9,7 @@ export default function TextInput(
 			className={cn(
 				`
         w-full p-3 bg-background-secondary text-white placeholder:text-content-placeholder rounded-xl 
-        border border-transparent hover:border-border-secondary hover:text-content-body active:border-border-tertiary
+        border border-transparent hover:border-border-secondary hover:text-content-body active:border-border-tertiary 
       `,
 				props.className,
 			)}
