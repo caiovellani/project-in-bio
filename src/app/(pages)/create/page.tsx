08d44beb5ed6f3f24aa869b6c518d5a3ef302506
Caio Vellani <caiovellani@outlook.com>
2025-01-13 22:01:28 -0300

    Refactor Button component and remove unused files
    - Update Button component in src/app/components/ui/button.tsx:
      - Change import for cn utility
      - Update type for children prop
      - Add props.className to className concatenation
      - Remove full width styling
    - Remove unused files:
      - src/app/globals.css
      - src/app/layout.tsx
      - src/app/page.tsx
    
    This commit streamlines the Button component and removes files that are no longer needed in the project structure.

---
+++ src/app/(pages)/create/page.tsx
@@ -0,0 +1,26 @@
+import { Rocket } from "lucide-react";
+import Header from "../../components/landing-page/header";
+import TextInput from "../../components/ui/text-input";
+import Button from "../../components/ui/button";
+
+export default function CreatePage() {
+	return (
+		<div>
+			<Header />
+			<div className="h-screen flex flex-col gap-10 items-center justify-center max-w-xl mx-auto">
+				<div className="flex items-center gap-4">
+					<h1 className="text-4xl font-bold text-white">Escolha seu link</h1>
+					<Rocket className="size-10" />
+				</div>
+				<form className="w-full flex items-center gap-2" action="">
+					<span className="text-white">projectinbio.com/</span>
+					<TextInput />
+					<Button>Criar</Button>
+				</form>
+				<div className="">
+					<span className="text-accent-pink">Erro de exemplo</span>
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -1,22 +1,22 @@
-import type { ReactNode } from "react";
-import { cn } from "../../lib/utils";
+import { cn } from "@/app/lib/utils";
 
 export default function Button({
 	children,
 	variant = "primary",
 	...props
 }: {
-	children: ReactNode;
+	children: React.ReactNode;
 	variant?: "primary" | "secondary" | "ghost";
 } & React.ButtonHTMLAttributes<HTMLButtonElement>) {
 	return (
 		<button
 			{...props}
 			className={cn(
-				"p-3 text-white rounded-xl font-bold whitespace-nowrap hover:opacity-95 disabled:opacity-70 focus:outline-none focus:ring-2 focus:ring-purple-600 w-full",
+				"p-3 text-white rounded-xl font-bold whitespace-nowrap hover:opacity-95 disabled:opacity-70 focus:outline-none focus:ring-2 focus:ring-purple-600",
 				variant === "primary" && "bg-accent-purple",
 				variant === "secondary" && "bg-background-tertiary",
 				variant === "ghost" && "border-border-primary",
+				props.className,
 			)}
 		>
 			{children}
