17138c3102b444b81a5df6a89f5744efcbc1be62
Caio Vellani <caiovellani@outlook.com>
2025-01-24 17:32:03 -0300

    Remove default URL fallback in formatUrl function

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -42,9 +42,6 @@ export const compressImage = (file: File): Promise<File> => {
 };
 
 export function formatUrl(url: string) {
-	if (!url) {
-		return "http/localhost:3000";
-	}
 	const formattedUrl = url.startsWith("http") ? url : `https://${url}`;
 	return formattedUrl;
 }
