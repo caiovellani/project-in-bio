026379863c0faf1eae76e6452e605473314df4e6
Caio Vellani <caiovellani@outlook.com>
2025-01-29 13:04:33 -0300

    Add Cache-Control header to disable caching for all routes
    - Configure Next.js to set 'Cache-Control: no-store' header for all routes
    - This ensures that content is always fetched from the server and not cached
    - Implemented in next.config.ts using the 'headers' configuration option

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	async headers() {
+		return [
+			{
+				source: "/(.*)", // Aplica para todas as rotas
+				headers: [
+					{
+						key: "Cache-Control",
+						value: "no-store", // Não armazena em cache
+					},
+				],
+			},
+		];
+	},
+	/* outras opções de configuração */
 };
 
 export default nextConfig;
